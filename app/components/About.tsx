052502ac4396374b8110c976a761fb81b5aa737f
Phil Barto <phildbarto@gmail.com>
2025-12-01 07:23:37 -0500

    current color scheme

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -16,7 +16,7 @@ export default function About() {
 		<section id="about" className="py-12">
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
 				<div>
-					<h2 className="text-5xl md:text-5xl font-bold text-red-500 mb-6 font-fira-code">
+					<h2 className="text-5xl md:text-5xl font-bold mb-6 font-fira-code text-[#FFA500]">
 						<TypingText
 							text="Hi! I'm Phil."
 							repeat={false}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Blogs from "./components/Blogs";
 import ContactMe from "./components/ContactMe";
+import { BackgroundBeams } from "@/components/ui/shadcn-io/background-beams";
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 			<main className="min-h-screen flex flex-col items-center justify-center">
 				<div className="w-full px-6">
 					<div className="w-4/7 md:w-4/7 mx-auto">
+						<BackgroundBeams className="absolute inset-0" />
 						<Header />
 						<About />
 						<Blogs />
